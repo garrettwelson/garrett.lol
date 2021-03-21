1b7c5b68b9a4c7a7d7689acaa5e0ada702e755df
Garrett <garrettwelson@gmail.com>
2021-03-21 15:19:54 -0400

    Add build infrastructure, ready to deploy v0

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,12 @@ const rssPlugin = require('@11ty/eleventy-plugin-rss')
 const dateFilter = require('./src/filters/date-filter.js')
 const w3DateFilter = require('./src/filters/w3-date-filter.js')
 
+// Transforms
+const htmlMinTransform = require('./src/transforms/html-min-transform.js')
+
+// Create a helpful production flag
+const isProduction = process.env.NODE_ENV === 'production'
+
 module.exports = (config) => {
     // Add filters
     config.addFilter('dateFilter', dateFilter)
@@ -18,8 +24,13 @@ module.exports = (config) => {
         return sortPostsByDate(collection.getFilteredByGlob('./src/posts/*.md'))
     })
 
-    // Set directories to pass through to the dist folder
-    config.addPassthroughCopy('./src/static/*/*')
+    // Only minify HTML if we are in production because it slows builds _right_ down
+    if (isProduction) {
+        config.addTransform('htmlmin', htmlMinTransform)
+    }
+
+    // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
+    config.setUseGitIgnore(false)
 
     return {
         markdownTemplateEngine: 'njk',
